3aa189634a243eee2b5e5f018e225eb271abc2bd
madefromjames <madefromjames@gmail.com>
2024-07-03 09:09:15 +0100

    edit post

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,4 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    console.log('the script is working fine')
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    // Your code here
+    function submitHandler() {
+        console.log('xhcek');
+    }
+    
 });
